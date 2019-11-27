393bfadc0b8335f693186d5e5673ee93ad6584b6
Ayushi Bansal <ayushibansal.india@gmail.com>
2019-11-26 23:31:17 -0800

    encryption file

---
+++ src/Participant/Voter/encryptions.py
@@ -0,0 +1,20 @@
+import Crypto
+from Crypto import Random
+from Crypto.PublicKey import RSA
+import base64
+from Crypto.Cipher import PKCS1_v1_5 as csign
+
+def generate_keys():
+    # RSA modulus length must be a multiple of 256 and >= 1024
+    modulus_length = 256*4 # use larger value in production
+    private_key = RSA.generate(modulus_length, Random.new().read)
+    public_key = private_key.publickey()
+    return private_key, public_key
+
+def sign(privatekey,data):
+    cipher = csign.new(privatekey)
+    cipher_text = cipher.encrypt(data.encode())
+    return base64.b64encode(cipher_text)
+
+def verify(publickey,data,sign):
+     return publickey.verify(data, (int(base64.b64decode(sign)),))
