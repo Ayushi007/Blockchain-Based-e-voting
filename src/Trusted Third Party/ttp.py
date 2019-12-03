90eca869d20ac73744c7dadc95fad1b675bf76fb
krithi96 <kmurugavel@ucdavis.edu>
2019-12-02 22:38:04 -0800

    updated changes

--- src/Trusted Third Party/ttp.py
+++ src/Trusted Third Party/ttp.py
@@ -4,8 +4,13 @@ from threading import Thread
 import time
 import random
 import mysql.connector
-#variable to store validity of incoming voter
+
+HOST = '0.0.0.0'
+PORT_PUBLICKEY = 5320
+PORT_REGISTER = 5322
+
 valid = False
+
 def validate_voter(data):
 	e_voting_db = mysql.connector.connect(
   		host="localhost",
@@ -19,18 +24,19 @@ def validate_voter(data):
 	for res in result:
 		print(res)
 		#check for existence of tuple here (secret-reference)
+
 while(True):
-	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	    s.bind((HOST, PORT_PUBLICKEY))
-	    s.listen(10)
-	    (conn, (ip, port)) = s.accept()
-	    #tuple receive
-	    data = conn.recv(1024)
-	    #check in the shared database if it exists
-	    valid = validate_voter(data)
-	    #send accept and candidate list/ reject
-	    if valid:
-		#query candidate database to find public keys
-		#send ok to voter 
-		#send candidate list to choose from 
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.bind((HOST,PORT_PUBLICKEY))
+	s.listen(10)
+	(conn, (ip, port)) = s.accept()
+	#tuple receive
+	data = conn.recv(1024)
+	#check in the shared database if it exists
+	valid = validate_voter(data)
+	#send accept and candidate list/reject
+	if (valid):
+		#query candidate database to find public address of the candidates store in a dictonary.
+		#send ok and candidate public addresses and name "dictionary" to voter [1,{}] .
+		v = True
