e89764f3f5e7382aeb81a9e272a32053f9581469
Ayushi007 <ayubansal@ucdavis.edu>
2019-12-07 07:45:56 +0000

    EC Changes

--- src/Election Commission/electioncommission.py
+++ src/Election Commission/electioncommission.py
@@ -86,99 +86,116 @@ def listenRegistrationRequest():
             s.bind((HOST, PORT_REGISTER))
             s.listen(1)
             (conn, (ip, port)) = s.accept()
+            
+            status = conn.recv(1024)
+            participant = str(status, 'utf-8')
+            print("Participant is", participant)
+            
+            if(participant == 'Candidate'):
+                conn.send(bytes("Starting", 'utf-8'))
+                data = conn.recv(1024)
+                print("Received m", data)
+                m = str(data, 'utf-8')
+                
+                if m in candidate_id:
+                    connect_multi = bytes('multichaind survey@10.168.0.6:9543 -daemon', 'utf-8')
+                    conn.send(connect_multi)
+                    print("Sent command to Candidate to get added to multichain")
+
+                    cand_add = conn.recv(1024)
+                    cand_add = str(cand_add, 'utf-8')
+                    access_cmd = "multichain-cli survey grant "+ cand_add+" receive,send"
+                    print("Candidate address received")
+                    os.system(access_cmd)
+                    print("Granted receive permission to Candidate:", cand_add)
+                    cand_add_list.append(cand_add)
+                    continue
+                else :
+                    data = bytes('invalid', 'utf-8')
+                    conn.send(data)
+                    continue
+            
+            conn.send(bytes("Starting", 'utf-8'))
             data = conn.recv(1024)
             print("Received m", data)
             #m = data  #g^r or candidate id
             m = int(str(data, 'utf-8'))
             #m = int.from_bytes(m, byteorder='big')
             print("converted m", m)
-            #if it is a candidate
-            if m in candidate_id:
-                data = bytes('valid', 'utf-8')
-                conn.send(data)
-                
-                cand_add = conn.recv(1024)
-                cand_add = str(cand_add, 'utf-8')
-                access_cmd = "multichain-cli survey grant "+ cand_add+" receive,send"
-                print("Candidate address received")
-                os.system(access_cmd)
-                print("Granted receive permission to Candidate:", cand_add)
-                cand_add_list.append(cand_add)
-                #store in candidate database.
-                #creates public address and instantitates into multichain
-            elif(isnumeric(m)):
-                #connect to the voter you just received data from and send a random number c.
-                c = 1
-                data = bytes(str(c), 'utf-8')
-                #data = pickle.dumps(c)
-                #print("c generated is", c)
-                conn.send(data)
-                #receive data from vote
-                data1 = conn.recv(1024)
-                s = data1
-                print("Received s", s)
-                #compute stuff and verify stuff
-                #if match send ok to voter
-                #s = int.from_bytes(s, byteorder='big')
-                s = int(str(s, 'utf-8'))
-                print("converted s", s)
-                k = pow(g,s)%p
-                print("k is", k)
-                valid = False
-                for i in y:
-                    if ((m * i)%p == k):
-                        valid = True
-                        print("Valid Voter")
-                        break
-                if(not valid):
-                    invalidMsg = bytes('-1', 'utf-8')
-                    conn.send(invalidMsg)
-                    conn.close()
-                    continue
-                #receive hash message with private key encrypted from voter if sent ok
-                mess = conn.recv(1024)
-                #mess = conn.recvfrom(4096)
-                print("messageReceied", mess)
-                converted_mess = pickle.loads(mess)
-                print("signedMessage-converted", converted_mess)
-                h = converted_mess[0]
-                sig = converted_mess[1]
-                #h, sig = pickle.loads(mess)
-                #public key verification.
+            
+           
+            #connect to the voter you just received data from and send a random number c.
+            c = 1
+            data = bytes(str(c), 'utf-8')
+            #data = pickle.dumps(c)
+            #print("c generated is", c)
+            conn.send(data)
+            #receive data from vote
+            data1 = conn.recv(1024)
+            s = data1
+            print("Received s", s)
+            #compute stuff and verify stuff
+            #if match send ok to voter
+            #s = int.from_bytes(s, byteorder='big')
+            s = int(str(s, 'utf-8'))
+            print("converted s", s)
+            k = pow(g,s)%p
+            print("k is", k)
+            valid = False
+            for i in y:
+                if ((m * i)%p == k):
+                    valid = True
+                    print("Valid Voter")
+                    break
+            if(not valid):
+                invalidMsg = bytes('-1', 'utf-8')
+                conn.send(invalidMsg)
+                conn.close()
+                continue
+            #receive hash message with private key encrypted from voter if sent ok
+            mess = conn.recv(1024)
+            #mess = conn.recvfrom(4096)
+            print("messageReceied", mess)
+            converted_mess = pickle.loads(mess)
+            print("signedMessage-converted", converted_mess)
+            h = converted_mess[0]
+            sig = converted_mess[1]
+            #h, sig = pickle.loads(mess)
+            #public key verification.
 
-                for pks in public_keys:
-                    if(verify(pks,h,sig)):
-                        found_keys = True
-                        print("Voter's public key found")
-                        #random number generate - reference number
-                        ref_no = generateRandomNumber(6)
-                        print("Generated ref_number", ref_no)
-                        data_ref = bytes(str(ref_no), 'utf-8')
-                        #send reference number back to voter
-                        conn.send(data_ref)
-                        #store in shared database //secret message// (we can just send reference number its enough) and reference number
-                        #creates public address and instantitates into multichain and assigns an asset using multichain.
-                        break
-                if(not found_keys):
-                    print("Authentication failed")
-                    conn.close()
-                    continue
-                
-                init_multichain = conn.recv(1024)
-                connect_multi = bytes('multichaind survey@10.168.0.6:9543 -daemon', 'utf-8')
-                conn.send(connect_multi)
-                print("Sent command to Voter to get added to multichain")
-                
-                voter_add = conn.recv(1024)
-                v_add = str(voter_add, 'utf-8')
-                access_cmd = "multichain-cli survey grant "+ v_add+" receive,send"
-                print("Voter_address received")
-                os.system(access_cmd)
-                print("Granted receive permission to Voter:", v_add)
-                
-                send_token = "multichain-cli survey sendassettoaddress "+v_add+" token 1"
-                os.system(send_token)
-                print("Token given to Voter: ", v_add)
+            for pks in public_keys:
+                if(verify(pks,h,sig)):
+                    found_keys = True
+                    print("Voter's public key found")
+                    #random number generate - reference number
+                    ref_no = generateRandomNumber(6)
+                    print("Generated ref_number", ref_no)
+                    data_ref = bytes(str(ref_no), 'utf-8')
+                    #send reference number back to voter
+                    conn.send(data_ref)
+                    #store in shared database //secret message// (we can just send reference number its enough) and reference number
+                    #creates public address and instantitates into multichain and assigns an asset using multichain.
+                    break
+            if(not found_keys):
+                print("Authentication failed")
+                conn.close()
+                continue
+
+            init_multichain = conn.recv(1024)
+            connect_multi = bytes('multichaind survey@10.168.0.6:9543 -daemon', 'utf-8')
+            conn.send(connect_multi)
+            print("Sent command to Voter to get added to multichain")
+
+            voter_add = conn.recv(1024)
+            v_add = str(voter_add, 'utf-8')
+            access_cmd = "multichain-cli survey grant "+ v_add+" receive,send"
+            print("Voter_address received")
+            os.system(access_cmd)
+            print("Granted receive permission to Voter:", v_add)
+
+            send_token = "multichain-cli survey sendassettoaddress "+v_add+" token 1"
+            os.system(send_token)
+            print("Token given to Voter: ", v_add)
 
 
 #create_shared_database()
