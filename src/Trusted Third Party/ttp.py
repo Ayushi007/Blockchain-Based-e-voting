57dfbc9957dfa4898a7b841542b7a4a90ad759b8
Krithi <krithigadxb@gmail.com>
2019-12-08 02:01:29 +0000

    Trusted third party

--- src/Trusted Third Party/ttp.py
+++ src/Trusted Third Party/ttp.py
@@ -1,36 +1,68 @@
-import socket
-import pickle
-from threading import Thread
-import time
-import random
-import mysql.connector
-#variable to store validity of incoming voter
-valid = False
-def validate_voter(data):
-	e_voting_db = mysql.connector.connect(
-  		host="localhost",
-  		user="ayushi",
-  		passwd="yourpassword",
-  		database="e_voting_db"
-	)
-	mycursor = e_voting_db.cursor()
-	mycursor.execute("SELECT * FROM voters")
-	result = mycursor.fetchall()
-	for res in result:
-		print(res)
-		#check for existence of tuple here (secret-reference)
-while(True):
+	import socket
+	import pickle
+	from threading import Thread
+	import time
+	import random
+	import json
+	
+	HOST = '0.0.0.0'
+	PORT_VOTING = 5320
+	PORT_ELECTION = 5322
+	
+	valid = False
+	
+	voter_details =[]
+	
+	def listen_voting():
+	    while(True):
+	        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	        s.bind((HOST,PORT_ELECTION))
+	        s.listen(10)
+	        (conn, (ip, port)) = s.accept()
+	        voter_content = conn.recv(1024)
+	        open('voter_details.txt',w+)
+	        file_voter.write(str(voter_contents))
+	        file_voter.close()
+	        
+	        #open file and read the content in a tuple
+	        with open('voter_details.txt', 'r') as fp:
+	        	for i in fp.readlines():
+	            tmp = i.split(",")
+	        	voter_details.append((float(tmp[0]), float(tmp[1])))
+	            
+	        conn.send(bytes("Send Candidate details", 'utf-8'))
+	        candidate_list = conn.recv(1024)
+	
+	th_public=Thread(target=listen_voting)
+	th_public.start()
+	th_public.join(10)
+	
+	while(True):
 	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	    s.bind((HOST, PORT_PUBLICKEY))
+	    s.bind((HOST,PORT_VOTING))
 	    s.listen(10)
 	    (conn, (ip, port)) = s.accept()
-	    #tuple receive
-	    data = conn.recv(1024)
-	    #check in the shared database if it exists
-	    valid = validate_voter(data)
-	    #send accept and candidate list/ reject
-	    if valid:
-		#query candidate database to find public keys
-		#send ok to voter 
-		#send candidate list to choose from 
+	    mess = conn.recv(1024)
+	    print("messageReceied", mess)
+	    converted_mess = pickle.loads(mess)
+	    refno = converted_mess[0]
+	    hashmsg = converted_mess[1]
+	    for i in voter_details:
+	        if(voter_details[i][0] == refno):
+	            if(voter_details[i][1] ==hasmessah):
+	                conn.send(bytes(candidate_list, 'utf-8'))
+	            else:
+	                print("invalid voter")
+	                conn.send(bytes(str(0), 'utf-8'))
+	        else:
+	            print("invalid voter")
+            conn.send(bytes(str(0), 'utf-8'))
+
+
+
+
+
+
+
